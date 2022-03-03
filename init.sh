889c0607bc2089e5bfad312e4ed4eed54a942b0b
Zakaria Sarmini <zakaria.sarmini@gmail.com>
2022-03-03 21:13:38 +0100

    edited the init script

--- init.sh
+++ init.sh
@@ -1,3 +1,5 @@
 git submodule update --init --recursive && 
 git submodule foreach "(git checkout master; git pull)" &&
 cd the-key-technology-FE && npm isntall
+echo
+read -p "Press any key to continue..." -n1 -s
